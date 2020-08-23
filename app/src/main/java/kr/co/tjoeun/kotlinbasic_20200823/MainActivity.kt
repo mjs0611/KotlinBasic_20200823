65275af7ceda12672fa94b512ca6f1b752fa8bab
JunSeung Mo <mojuns@gmail.com>
2020-08-23 12:12:01 +0900

    입력된 문구를 받아서 토스트로 출력

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200823/MainActivity.kt
@@ -12,12 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         clickMeBtn.setOnClickListener {
-//            클릭미 버튼이 눌리면 실행될 코드 { }
+//        클릭미 버튼이 눌리면 실행될 코드 { }
 
             Log.d("메인화면", "클릭 미 버튼 눌림")
         }
 
-//      xml에 추가한 버튼을 makeLogBtn 으로 이름지어서 => 눌리면 "ㄹ그 남기기 버튼 눌림" 로그 남겨보기
+//        xml에 추가한 버튼을 makeLogBtn 으로 이름지어서 => 눌리면 "ㄹ그 남기기 버튼 눌림" 로그 남겨보기
 
         makeLogBtn.setOnClickListener {
             Log.d("메인화면", "로그 남기기 버튼 눌림")
@@ -25,8 +25,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         toastBtn.setOnClickListener {
-//          사용자에게 "버튼 눌림" 안내 문구 제공
+//        사용자에게 "버튼 눌림" 안내 문구 제공
             Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+
+//        입력한 문구를 받아서 => 토스트로 출력
+        contentToastBtn.setOnClickListener {
+//            contentEdt의 문구 (text 속성의 값) => (Sring으로) 받아서 => 코틀린 변수에 저장
+            val inputContent = contentEdt.text.toString()
+
+//            변수에 저장된 문구를 => 토스트의 재료로 사용
+            Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
         }
 
     }
